25c135693f02fce54a66a0d3d0a2b5950ac9515c
Whoppin <simon.lidman@hotmail.se>
2023-05-10 10:09:57 +0200

    small fix

--- Cycle.m
+++ Cycle.m
@@ -147,22 +147,6 @@ xlabel('Time [S]');
 ylabel('Hits');
 legend(plus(num2str(round(SV.Table.MPa)),"MPa"));
 grid on
-figure('name', 'A-E vs Stress 0-200kHz','Position',...
-    [60,60,1400,700])
-hold on
-for k = 1 : exp
-%     yyaxis left
-%     plot(SV.time2{k}/SV.Resolution(k), SV.SpreadEner{k});
-    plot(SV.Fun_TensileStress{k}, SV.SpreadEner1{k});
-%     yyaxis right
-%     plot(SV.Fun_Time{k}, SV.Fun_TensileStress{k})
-end
-title('Cumulative Acoustic Energy vs Stress 0-200kHz');
-% xlim([0 max(SV.Fun_Time)]);
-% xlabel('Time [s]');
-xlabel('Stress [MPa]');
-ylabel('Energy [aJ]');
-legend(num2str(round(SV.Table.MPa)));
 hold off
 
 run Plots_cycle/A_E_Stress_0_200kHz.m
