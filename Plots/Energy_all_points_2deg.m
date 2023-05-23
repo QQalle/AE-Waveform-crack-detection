9c802ebcca810c12ad3a6bcf19da5a76dba04b08
QQalle <calle.liljeberg@gmail.com>
2023-05-23 14:37:37 +0200

    Made some changes to crack energy graphs

--- Plots/Energy_all_points_2deg.m
+++ Plots/Energy_all_points_2deg.m
@@ -7,9 +7,6 @@
 % p_cracks = [87, 79, 13, 1, 3, 0, 0];
 cracks = [106.5, 100.5, 28.5, 3.5, 4, 0, 0];
 
-point_one = 4;
-point_two = 2;
-
 no_tabs = 0;
 
 energy_data = table2array(SV.EnergyTable(1:(end-no_tabs), 2:4));
@@ -37,17 +34,16 @@ for t = energy_titles
         'Position',[60,60,1400,700]);
     hold on
 
-    lf_mc_en = polyfit([e(point_one) e(point_two)],...
-        [cracks_data(point_one) cracks_data(point_two)], 1);
+    lf_mc_en = polyfit(e, cracks_data, 1);
     % lcp = polyfit(x, p_cracks, 1);
     lff_mc_en = polyval(lf_mc_en, [min(e) max(e)]);
     % fp = polyval(lcp, x);
 
     scatter(e, cracks_data, 50, "filled", "Color", "#0072BD");
 
-    scatter([e(point_two) e(point_one)],...
-        [cracks_data(point_two) cracks_data(point_one)],...
-        100, [0.4660 0.6740 0.1880], "o", "LineWidth", 1.5);
+    %scatter([e(point_two) e(point_one)],...
+    %   [cracks_data(point_two) cracks_data(point_one)],...
+    %   100, [0.4660 0.6740 0.1880], "o", "LineWidth", 1.5);
 
     plot([min(e) max(e)], lff_mc_en, "LineStyle", "--", "Color", "#0072BD", ...
         "LineWidth", 2);
@@ -106,7 +102,7 @@ for t = energy_titles
     title(append("Matrix Cracks vs Max Stress, using two points of data", captext));
     xlabel('Max Stress [MPa]');
     ylabel('Average amount of Matrix Cracks per side');
-    legend(["Real matrix crack data", "Expected number off cracks (calculated using matrix crack data from two specimen)"], ...
+    legend(["Real matrix crack data", "Expected number off cracks (calculated using matrix crack data from seven specimens)"], ...
         'location','southoutside');
     grid on
     set(gca,'FontSize',14)
